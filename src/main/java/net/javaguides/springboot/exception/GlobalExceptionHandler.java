c1c33eb8cb3c75e0e64e0adf09ecbde85a85301c
Christine Lee <wonhwalee52@gmail.com>
2022-07-03 18:03:32 -0400

    Add employee management system project

---
+++ src/main/java/net/javaguides/springboot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+//What is use of @ControllerAdvice?
+//@ControllerAdvice is a specialization of the @Component annotation which allows
+//to handle exceptions across the whole application in one global handling component.
+//It can be viewed as an interceptor of exceptions thrown by methods annotated with
+//@RequestMapping and similar
+
+package net.javaguides.springboot.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> globalExceptionHandler(Exception ex, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
